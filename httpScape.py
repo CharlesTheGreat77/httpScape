7fba4fbdeb1f97bc8862d62f2970dcc80e5e6eb2
Charles The Great <27988707+CharlesTheGreat77@users.noreply.github.com>
2023-06-22 10:30:41 -0700

    Update httpScape.py

--- httpScape.py
+++ httpScape.py
@@ -1,70 +1,81 @@
-import requests, os, random, argparse, time
-from threading import Thread
+import requests
+import threading
+from queue import Queue
+import argparse
 
-class httpScape:
-    def __init__(self):
+class ProxyScraper:
+    def __init__(self, max_valid_proxies):
         self.pList = []
-        self.proxies = []
-        self.threads = []
-        self._COUNTER = 1
-        self._MAX_PROXIES = 0
-        self._VERBOSE = ''
-        
+        self.threads = 0
+        self.max_valid_proxies = max_valid_proxies
+        self.valid_proxies = []
+        self.proxy_lock = threading.Lock()
+        self.exit_event = threading.Event()
+
     def scrape(self):
         request = requests.get('https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all').text
         for proxy in request.split():
-            self.pList.append({'http':f'http://{proxy}', 'https': f'http://{proxy}'})
-    
-    def check(self):
-        while self._COUNTER < self._MAX_PROXIES:
-            proxy = random.choice(self.pList)
-            addr = list(proxy.values())
-            server = addr[0]
-            ip = server.replace('http://', '')
-            if ip in self.proxies:
-                self.check()
-            try:
-                response = requests.get('https://google.com/', timeout=5, proxies=proxy)
-                if response.status_code == 200:
-                    self.proxies.append(ip)
-                    self._COUNTER += 1
-                    if self._VERBOSE:
-                        print(f'[\u2705] {ip}')
-                if self._COUNTER >= len(self.pList):
-                    break
-            except:
-                self.check()
-                
-    def main(self):
-        parser = argparse.ArgumentParser(description='HTTP Proxy Scraper/Checker Framework')
-        parser.add_argument('-m', '--maximum', help='specify max amount of proxies', type=int, required=False)
-        parser.add_argument('-o', '--output', help='specify output file name [default:proxies.txt]', type=str, default='proxies.txt')
-        parser.add_argument('-t', '--threads', help='specify amount of threads [default:5]', type=int, default=5)
-        parser.add_argument('-v', '--verbose', help='enable verbosity', action='store_true')
-        args = parser.parse_args()
-        
-        self._MAX_PROXIES = args.maximum
-        output = args.output
-        numThreads = args.threads
-        self._VERBOSE = args.verbose
-        cwd = os.getcwd()
-        
-        self.scrape()
-        print(f'[*] {len(self.pList)} Proxies found..\n')
-        print(f'[*] Checking connectivity of {self._MAX_PROXIES} proxies..')
-        start = time.time()
-        for index in range(numThreads):
-            t = Thread(target=self.check, daemon=True)
-            self.threads.append(t)
+            self.pList.append({'http': f'http://{proxy}', 'https': f'http://{proxy}'})
+
+    def validate_proxy(self, proxy):
+        try:
+            response = requests.get('https://google.com', proxies=proxy, timeout=3)
+            if response.status_code == 200:
+                with self.proxy_lock:
+                    self.valid_proxies.append(proxy)
+                    if len(self.valid_proxies) >= self.max_valid_proxies:
+                        self.exit_event.set()
+                print(f"Valid proxy: {proxy['http']}")
+        except (requests.RequestException, ValueError):
+            pass
+
+    def queue_proxies(self, thready):
+        num_threads = thready  # Number of threads for validation
+        proxy_queue = Queue()
+
+        for proxy in self.pList:
+            proxy_queue.put(proxy)
+
+        def worker():
+            while not proxy_queue.empty() and not self.exit_event.is_set():
+                proxy = proxy_queue.get()
+                self.validate_proxy(proxy)
+                proxy_queue.task_done()
+
+        threads = []
+        for _ in range(num_threads):
+            t = threading.Thread(target=worker)
             t.start()
-        for index, thread in enumerate(self.threads):
+            threads.append(t)
+
+        while not proxy_queue.empty() and not self.exit_event.is_set():
+            self.exit_event.wait(timeout=1)
+
+        self.exit_event.set()
+        for t in threads:
             t.join()
-        end = time.time()
-        laps = end - start
 
-        print(f'[*] Saving proxies to {output} | Time: {int(laps)} seconds')  
-        with open(f'{cwd}/{output}', 'w') as file:
-            file.write('\n'.join(self.proxies))
+    def get_valid_proxies(self):
+        return self.valid_proxies[:self.max_valid_proxies]
 
-scrape = httpScape()
-scrape.main()
+def main():
+        parser = argparse.ArgumentParser(description='Proxy Scraper')
+        parser.add_argument('--outfile', '-o', help='Output file name')
+        parser.add_argument('--max-valid', '-m', type=int, default=10, help='Maximum number of valid proxies')
+        parser.add_argument('--threads', '-t', type=int, default=5, help='number of threads')
+        args = parser.parse_args()
+        thready = args.threads
+
+        proxy_scraper = ProxyScraper(args.max_valid)
+        proxy_scraper.scrape()
+        proxy_scraper.queue_proxies(thready)
+        valid_proxies = proxy_scraper.get_valid_proxies()
+
+        if args.outfile:
+            with open(args.outfile, 'w') as f:
+                for proxy in valid_proxies:
+                    proxy = proxy['http']
+                    proxy = proxy.replace('http://','')
+                    f.write(f"{proxy}\n")
+
+main()
